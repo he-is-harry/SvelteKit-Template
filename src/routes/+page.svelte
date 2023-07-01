047a0b0979b76d9ff8a5aa8d93860e369db84061
he-is-harry <harryhe_@hotmail.com>
2023-07-01 19:28:06 -0400

    Added template

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    const name: string = "Harry";
+</script>
+
+<div class="two-columns">
+    <div class="center-columns green-border">
+        <h1>Hello {name}, to SvelteKit!</h1>
+        <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    </div>
+
+    <div class="center-columns orange-border">
+        <h1>Hello {name}, to SvelteKit!</h1>
+        <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    </div>
+</div>
